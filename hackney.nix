775474d251fa75f1d82439f4ba3b9ad9636ece45
Antony O'Neill <antony@ant-web.co.uk>
2020-12-17 15:47:25 +0000

    Add Hackney nix shell

---
+++ hackney.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.dotnet-sdk_3
+    pkgs.jetbrains.rider
+    pkgs.jetbrains.webstorm
+    pkgs.jetbrains.datagrip
+    pkgs.gitAndTools.gh
+    pkgs.heroku
+    pkgs.awscli2
+    pkgs.ssm-session-manager-plugin
+    pkgs.vscode
+  ];
+
+  shellHook = ''
+    export DOTNET_CLI_TELEMETRY_OPTOUT=true
+    export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
+    export PATH=$PATH:$HOME/.dotnet/tools
+
+    export HEROKU_ORGANIZATION=hackney-apps
+    
+    export AWS_PROFILE=h_dev
+    export RPROMPT="%F{green}hackney(\$AWS_PROFILE)"
+  '';
+}
