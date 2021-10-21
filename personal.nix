e4af4007176a16013e2b3b22a25d9c4236af1a2c
Antony O'Neill <antony@ant-web.co.uk>
2021-10-21 14:19:02 +0100

    Remove jetbrains from nix

--- personal.nix
+++ personal.nix
@@ -1,33 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = [
-    pkgs.dotnet-sdk_3
-    pkgs.jetbrains.rider
-    pkgs.jetbrains.webstorm
-    pkgs.jetbrains.datagrip
     pkgs.gitAndTools.gh
-    pkgs.heroku
     pkgs.awscli2
     pkgs.ssm-session-manager-plugin
     pkgs.terraform_0_13
+    pkgs.jdk11_headless
   ];
 
   shellHook = ''
-    export DOTNET_CLI_TELEMETRY_OPTOUT=true
-    export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
-    export PATH=$PATH:/home/antony/.dotnet/tools
-
     export WORK_ORG=ant
     export RPROMPT="%F{blue}$WORK_ORG"
-
-    if [[ "$(dotnet tool list -g|grep amazon.lambda.tools | wc -l)" -eq 0 ]]
-    then
-      dotnet tool install -g Amazon.Lambda.Tools
-    fi
-
-    if [[ "$(dotnet new --list |grep AWS/Lambda | wc -l)" -eq 0 ]]
-    then
-      dotnet new --install Amazon.Lambda.Templates::4.2.0
-    fi
   '';
 }
